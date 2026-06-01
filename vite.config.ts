6ac8417a261f23587995d20dff377fb05baa21bc
maaddahriyah0-com <maaddahriyah0@gmail.com>
2026-06-01 19:49:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rekap-Nilai-Ijazah-Mts/', // Pastikan namanya sesuai dengan repositori yang baru!
+  base: '/Rekap-Nilai-Ijazah-Mts/',
 })
